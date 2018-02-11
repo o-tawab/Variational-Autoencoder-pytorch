7f9126fb6745b06900968d2e2e6e9e8a170d5e33
3ammor <omar.tawab95@gmail.com>
2018-02-11 15:55:24 +0200

    loss class

---
+++ loss.py
@@ -0,0 +1,22 @@
+from torch import nn
+from torch.autograd import Variable
+
+
+class Loss(nn.Module):
+    def __init__(self):
+        super(VAE, self).__init__()
+        self.ce_loss = torch.nn.CrossEntropyLoss(size_average=False)
+
+    def forward(self, recon_x, x, mu, logvar):
+        # BCE = F.mse_loss(recon_x, x, size_average=False)
+        x = x * 255
+        x.data = x.data.int().long().view(-1)
+        recon_x = recon_x.view(-1, 256)
+
+        CE = self.ce_loss(recon_x, x)
+
+        # see Appendix B from VAE paper:    https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return CE + KLD

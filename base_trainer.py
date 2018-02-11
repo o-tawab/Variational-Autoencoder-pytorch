47e358867fc322b87db4e8f63bfe2968b333f699
3ammor <omar.tawab95@gmail.com>
2018-02-11 15:55:06 +0200

    new file structure

---
+++ base_trainer.py
@@ -0,0 +1,71 @@
+import shutil
+
+import torch.nn as nn
+import torch.optim
+from tensorboardX import SummaryWriter
+from torch.autograd import Variable
+from torch.optim.rmsprop import RMSprop
+from tqdm import tqdm
+
+from utils import AverageTracker
+
+
+class BaseTrainer:
+    def __init__(self, model, loss, train_loader, test_loader, args):
+        self.model = model
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.args = args
+        self.start_epoch = 0
+
+        # Loss function and Optimizer
+        self.loss = loss
+        self.optimizer = None
+
+        # Model Loading
+        if args.resume:
+            self.load_checkpoint(self.args.resume_from)
+
+        # Tensorboard Writer
+        self.summary_writer = SummaryWriter(log_dir=args.summary_dir)
+
+    def train(self):
+        raise NotImplementedError
+
+    def test(self, cur_epoch):
+        raise NotImplementedError
+
+    def get_optimizer(self):
+        raise NotImplementedError
+
+    def adjust_learning_rate(self, optimizer, epoch):
+        """Sets the learning rate to the initial LR multiplied by 0.98 every epoch"""
+        learning_rate = self.args.learning_rate * (self.args.learning_rate_decay ** epoch)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = learning_rate
+
+    def save_checkpoint(self, state, is_best, filename='checkpoint.pth.tar'):
+        '''
+        :param state: {'epoch': cur_epoch + 1, 'state_dict': self.model.state_dict(),
+                            'optimizer': self.optimizer.state_dict()}
+        :param is_best:
+        :param filename:
+        :return:
+        '''
+        torch.save(state, self.args.checkpoint_dir + filename)
+        if is_best:
+            shutil.copyfile(self.args.checkpoint_dir + filename,
+                            self.args.checkpoint_dir + 'model_best.pth.tar')
+
+    def load_checkpoint(self, filename):
+        filename = self.args.checkpoint_dir + filename
+        try:
+            print("Loading checkpoint '{}'".format(filename))
+            checkpoint = torch.load(filename)
+            self.start_epoch = checkpoint['epoch']
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("Checkpoint loaded successfully from '{}' at (epoch {})\n"
+                  .format(self.args.checkpoint_dir, checkpoint['epoch']))
+        except:
+            print("No checkpoint exists from '{}'. Skipping...\n".format(self.args.checkpoint_dir))
